3b9df283657be95d6e4e29e7053182da4b1b6e2f
Emily Nozaki <emiri3128.gmail.com>
2022-03-03 16:57:38 +0900

    Initial Commit

---
+++ Purari/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Purari
+//
+//  Created by Emily Nozaki on 2022/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
